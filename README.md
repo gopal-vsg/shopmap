a2c3dfcc0fe7b321e04edd6709997b3bdae47816
venuwiz <140985360+venuwiz@users.noreply.github.com>
2024-07-17 15:29:45 +0530

    skeleton svelte app with daisyui

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# shopmap
+ShopMap Web App
+
+Please consult the following tutorials for contributing to this app.
+
+Framework: https://learn.svelte.dev/tutorial/welcome-to-svelte
+UI: https://daisyui.com/components/
+Maps: https://leafletjs.com/examples/quick-start/
+
+Use pnpm for package management
---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <div class="card">
+    <Counter />
+  </div>
+
+  <div class="flex flex-row">
+    <div class="flex flex-col">
+      <div>One
+        <button class="btn btn-primary">Click me</button>
+        <button class="btn btn-secondary">Click me</button>
+        <button class="btn btn-tertiary">Click me</button>
+        <button class="btn btn-sm btn-tertiary">Click me</button>
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <div>Three</div>
+      <div>Four</div>
+      <div>Five</div>
+    </div>
+  </div>
+</main>
+
+<style>
+</style>
