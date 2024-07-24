8ce67b72f08adcb07e5c023356ad981b5ab2b0bf
gopal-vsg <saigopalvallu7@gmail.com>
2024-07-24 09:09:09 +0530

    the Login and logout feature added

--- src/App.svelte
+++ src/App.svelte
@@ -1,69 +1,100 @@
+<!--App.svelte-->
 <script lang="ts">
   import Shops from "./components/Shops.svelte";
   import Orders from "./components/Orders.svelte";
   import Leaflet from "./components/Leaflet.svelte";
   import PlaceOrder from "./components/PlaceOrder.svelte";
   import CreateShop from "./components/CreateShop.svelte";
-  let selectedOption = "hello";
+  import Login from "./components/Login.svelte";
+  import { onMount } from "svelte";
+  import { supabase } from "./lib/supabase";
+  let selectedOption = "shops";
+  let user = null;
+
+  onMount(async()=>{
+    const { data : { user: currentuser }} = await supabase.auth.getUser();
+    user = currentuser;
+    if(!user){
+      const loginDialog = document.getElementById('loginDialog') as HTMLDialogElement;
+      loginDialog.showModal();
+    }
+  })
+
+  async function signOut() {
+    await supabase.auth.signOut();
+    user = null;
+    const loginDialog = document.getElementById('loginDialog') as HTMLDialogElement;
+    location.reload()
+    loginDialog.showModal();
+    
+  }
+
 </script>
 
-<main class="min-h-screen bg-gray-100">
-  <nav class="bg-blue-500 p-4 shadow-md sp flex justify-between items-center">
-    <!-- svelte-ignore a11y-invalid-attribute -->
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <a class="text-white text-2xl font-bold">Shop Map</a>
-    <div class="flex space-x-4">
-      <button class="btn" onclick="PlaceOrder.showModal()">Add shop</button>
-      <button class="btn" onclick="PlaceShop.showModal()">Place order</button>
-    </div>
-    
-  </nav>
 
-  <Leaflet />
-  
-  <div class="container mx-auto mt-8">
-    <div class="w-3/4 mx-auto">
-      <select 
-        bind:value={selectedOption}
-        class="w-24 p-2 mb-4 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-      >
-        <option value="hello">Shops</option>
-        <option value="world">Orders</option>
-      </select>
-      
-      <div class="bg-white rounded-lg shadow-md p-6">
-        {#if selectedOption === "hello"}
-          <Shops />
-        {:else}
-          <Orders />
-        {/if}
+<main class="min-h-screen bg-gray-100">
+  {#if user}
+    <nav class="bg-blue-500 p-4 shadow-md sp flex justify-between items-center">
+      <a class="text-white text-2xl font-bold">Shop Map</a>
+      <div class="flex space-x-4">
+        <button class="btn" onclick="PlaceOrder.showModal()">Add shop</button>
+        <button class="btn" onclick="PlaceShop.showModal()">Place order</button>
+        <button class="btn" on:click={signOut}>Logout</button>
+      </div>
+    </nav>
+
+    <Leaflet />
+    
+    <div class="container mx-auto mt-8">
+      <div class="w-3/4 mx-auto">
+        <select 
+          bind:value={selectedOption}
+          class="w-24 p-2 mb-4 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+        >
+          <option value="shops">Shops</option>
+          <option value="orders">Orders</option>
+        </select>
+        
+        <div class="bg-white rounded-lg shadow-md p-6">
+          {#if selectedOption === "shops"}
+            <Shops />
+          {:else}
+            <Orders />
+          {/if}
+        </div>
       </div>
     </div>
-  </div>
-  <!-- You can open the modal using ID.showModal() method -->
 
-<dialog id="PlaceShop" class="modal">
-  <div class="modal-box">
-    <form method="dialog">
-      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    <PlaceOrder/>
-  </div>
-  <form method="dialog" class="modal-backdrop">
-    <button>close</button>
-  </form>
-</dialog>
-<dialog id="PlaceOrder" class="modal">
-  <div class="modal-box">
-    <form method="dialog">
-      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    <CreateShop/>
-  </div>
-  <form method="dialog" class="modal-backdrop">
-    <button>close</button>
-  </form>
-</dialog>
+    <dialog id="PlaceShop" class="modal">
+      <div class="modal-box">
+        <form method="dialog">
+          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <PlaceOrder/>
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+
+    <dialog id="PlaceOrder" class="modal">
+      <div class="modal-box">
+        <form method="dialog">
+          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <CreateShop/>
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+  {:else}
+    <dialog id="loginDialog" class="modal">
+      <div>
+        <Login/>
+      </div>
+    </dialog>
+  {/if}
 </main>
 
 <style>
---
+++ src/components/Login.svelte
@@ -0,0 +1,69 @@
+<!--Login.svelte-->
+<script lang="ts">
+    import { supabase } from "../lib/supabase";
+
+    let email = "";
+    let password = "";
+    let errormessage = "";
+    async function signInWithEmail() {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) {
+        errormessage = error.message;
+      } else {
+        const loginDialog = document.getElementById("loginDialog") as HTMLDialogElement;
+        loginDialog.close();
+        location.reload();
+      }
+    }
+  </script>
+  
+  <main class="flex flex-col items-center justify-center min-h-screen ">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title text-2xl font-bold mb-6 text-center">Login</h2>
+        <form on:submit|preventDefault={signInWithEmail} class="space-y-4">
+          <label class="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              class="h-4 w-4 opacity-70"
+            >
+              <path
+                d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
+              />
+              <path
+                d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
+              />
+            </svg>
+            <input type="text" class="grow" placeholder="Email" bind:value={email}/>
+          </label>
+          <label class="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              class="h-4 w-4 opacity-70"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <input type="password" class="grow" placeholder="Password" bind:value={password}/>
+          </label>
+          <button type="submit" class="btn btn-info w-32">Login</button>
+        </form>
+        {#if errormessage}
+          <div class="alert alert-error mt-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <span>{errormessage}</span>
+          </div>
+        {/if}
+      </div>
+    </div>
+  </main>
